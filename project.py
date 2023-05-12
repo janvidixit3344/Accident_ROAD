e5bbfd5b079ebfae344fa862040e8c1633f9b5ca
Abhishek Kumar <abhimisra50@gmail.com>
2023-05-12 08:48:30 +0530

    graph2.html

--- project.py
+++ project.py
@@ -16,12 +16,21 @@ def index():
 
 @app.route('/graph/1')
 def graph_1():
-    acc_mode = load_data()
+    grouped_obj = load_data()
     year = [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014]
-    fig1 = px.area(acc_mode, x=acc_mode.index, y='Total', title='Accident year-wise')
-    fig2 = px.bar(acc_mode, x=acc_mode.index, y='Total', title='Accident year-wise')
-    fig3 = px.bar(acc_mode, x=acc_mode.index, y='Total', title='Accident year-wise')
-    return render_template('graph1.html', fig1=fig1.to_html(),fig2=fig2.to_html(), fig3=fig3.to_html())
+    fig = px.area(grouped_obj, x='State/UT', y='Total', title='Accident year-wise')
+    conclusion = 'The graph shows the accidents Statewise.'
+    fig.add_annotation(text=conclusion, xref='paper', yref='paper', x=.5, y=1, bgcolor='grey', showarrow=False, font=dict(size=12))
+    
+    #fig2 = fig = px.area(acc_mode, x=acc_mode.index, y='Total', title='Accident year-wise')
+    #conclusion = 'The graph shows accidents from every State and UT.'
+    #fig.add_annotation(text=conclusion, xref='paper', yref='paper', x=.5, y=1, bgcolor='grey', showarrow=False, font=dict(size=12))
+    
+    #fig3 = ['Bicycle', 'Bus','Car','Jeep','Other Motor Vehicles','Others','Pedestrian','Three Wheeler','Truck/Lorry','Two Wheeler']
+    #fig3 = px.bar(acc_mode, x=acc_mode.index, y='Total', title='Accident year-wise')
+    #conclusion = 'The graph shows that accidents due to vehicles involved.'
+    #fig.add_annotation(text=conclusion, xref='paper', yref='paper', x=.5, y=1, bgcolor='grey', showarrow=False, font=dict(size=12))
+    return render_template('graph1.html', fig1=fig.to_html())#,fig2=fig2.to_html(), fig3=fig3.to_html())
 
 if(__name__ == '__main__'):
     app.run(host='0.0.0.0', port=8080, debug=True)
